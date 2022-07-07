2ee5317b106a0bc352325cd62d2098cebb57a293
Anup Mahato <anupmahato033@gmail.com>
2022-07-07 20:21:46 +0530

    added showdon lib

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,48 @@
 import { useState, useRef } from "react";
 import "./App.css";
-import Header from "./views/Header/Header";
+// import Header from "./views/Header/Header";
 import Editor, { DiffEditor, useMonaco, loader } from "@monaco-editor/react";
+import { debounce } from "lodash";
+
+///showdown
+import Showdown from "showdown";
+
+import "./views/Header/Header.css";
 
 function App() {
+  const DEFAULT_EDITOR_CODE = "// some code";
   const [count, setCount] = useState(0);
   const [editorCode, setEditorCode] = useState("");
   const editorRef = useRef(null);
+  const [defaultValue, setDefaultvalue] = useState(DEFAULT_EDITOR_CODE);
+  const mdHtml = useRef(null);
 
   function handleEditorDidMount(editor, monaco) {
     editorRef.current = editor;
   }
+
+  const debounce_handleEditorChange = debounce(handleEditorChange, 1000);
+
   function handleEditorChange(value, event) {
-    console.log(value);
-    console.log(event);
+    const converter = new Showdown.Converter();
+    const html = converter.makeHtml(value);
+    setEditorCode(html);
+  }
+
+  function handleClear() {
+    setDefaultvalue(DEFAULT_EDITOR_CODE);
+    setEditorCode(null);
   }
 
   return (
     <div className="App">
-      <Header />
+      {/* header */}
+      <header className="app__header">
+        <nav>
+          <button>Home</button>
+          <button onClick={handleClear}>Clear All</button>
+        </nav>
+      </header>
       {/* editor */}
       <div className="app__content">
         <div className="editor">
@@ -28,16 +52,28 @@ function App() {
             height="100%"
             width="100%"
             defaultLanguage="markdown"
-            defaultValue="// some comment"
+            defaultValue={defaultValue}
             onMount={handleEditorDidMount}
-            onChange={handleEditorChange}
+            onChange={debounce_handleEditorChange}
           />
         </div>
-        <div className="preview">{editorCode}</div>
+        <div
+          className="preview"
+          dangerouslySetInnerHTML={{ __html: editorCode }}
+        ></div>
       </div>
-
-      {/* preview */}
     </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="app__header">
+      <nav>
+        <button>Home</button>
+        <button>Clear All</button>
+      </nav>
+    </header>
   );
 }
 
